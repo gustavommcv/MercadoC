dd3e2f97fafcd0981b2f964dca6a088b461508d0
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-12 20:44:20 -0300

    Estruturando projeto

--- mecado.c
+++ mecado.c
@@ -1,9 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+typedef struct {
+    int codigo;
+    char nome[30];
+    float preco;
+} Produto;
+
+typedef struct {
+    Produto produto;
+    int quantidade;
+} Carrinho;
+
+void infoProduto(Produto prod);
+void menu();
+void cadastrarProduto();
+void listarProdutos();
+void visualizarCarrinho();
+Produto pegarProdutoPorCodigo(int codigo);
+int* temNoCarrinho(int codigo);
+void fecharPedido();
+
+static int contador_produto = 0;
+static int contador_carrinho = 0;
+static Carrinho carrinho[50];
+static Produto produtos[50];
 
 int main(void) {
 
 
-
     return 0;
+}
+
+void infoProduto(Produto prod) {
+
+}
+
+void menu() {
+    
+}
+
+void cadastrarProduto() {
+
+}
+
+void listarProdutos() {
+
+}
+
+void visualizarCarrinho() {
+
+}
+
+Produto pegarProdutoPorCodigo(int codigo) {
+
+}
+
+int* temNoCarrinho(int codigo) {
+
+}
+
+void fecharPedido() {
+
 }
